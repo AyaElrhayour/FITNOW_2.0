8971bd1fd9cf76333f8a3327ce6bf93f9eff7079
Aya <ayaelrhayour123@gmail.com>
2024-04-04 14:36:30 +0000

    project

---
+++ FITNOW/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Navbar';
+import HeroSection from './HeroSection';
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+    </div>
+  );
+}
